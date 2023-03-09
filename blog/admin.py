b6ee612fe00339b05734b00ae78fcef743dd814f
Nadia HYLARY <nhylary12@gmail.com>
2023-03-09 20:33:44 +0100

    added the models with relations with admin configuration

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Author, Tag
+
+
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("date_created", )
+    list_display = ("title", "author", "date_created")
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
